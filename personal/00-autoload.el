6e264559e01f36d235d3fa1ac4eac64a2cb215cf
Lee Hall <lhall@javafoundry.com>
2022-01-04 21:32:54 -0500

    Auto modes

--- personal/00-autoload.el
+++ personal/00-autoload.el
@@ -1,1 +1,3 @@
-(prelude-require-packages '(jq-mode jq-format restclient pyenv-mode pyenv-mode-auto))
+(prelude-require-packages '(jq-mode jq-format restclient pyenv-mode pyenv-mode-auto auto-minor-mode))
+(add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))
+(add-to-list 'auto-minor-mode-alist '("\\.py\\'" . pyenv-mode))
