18239acdde667771d4359052d0867336e257fd6a
Lee Hall <lhall@javafoundry.com>
2022-01-07 07:07:21 -0500

    formatting

--- personal/00-autoload.el
+++ personal/00-autoload.el
@@ -1,3 +1,4 @@
-(prelude-require-packages '(jq-mode jq-format restclient pyenv-mode pyenv-mode-auto auto-minor-mode))
+(prelude-require-packages '(jq-mode jq-format restclient pyenv-mode
+                                    pyenv-mode-auto auto-minor-mode))
 (add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))
 (add-to-list 'auto-minor-mode-alist '("\\.py\\'" . pyenv-mode))
