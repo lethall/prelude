ca80413463c5190bd29e9bdd01e1e7ca0bb5592e
Lee Hall <lhall@javafoundry.com>
2022-01-16 21:48:16 -0500

    Multi cursor

--- personal/00-autoload.el
+++ personal/00-autoload.el
@@ -1,4 +1,4 @@
 (prelude-require-packages '(jq-mode jq-format restclient pyenv-mode
-                                    pyenv-mode-auto auto-minor-mode))
+                      pyenv-mode-auto auto-minor-mode multiple-cursors))
 (add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))
 (add-to-list 'auto-minor-mode-alist '("\\.py\\'" . pyenv-mode))
